6ce643a4243d5a8d002c74afdd40d8d11fa2059d
arkatebi <arkatebi@gmail.com>
2016-08-22 12:48:38 -0500

    updated filter methods to print GO terms in the map file

--- xTract_sp_trainingSet.py
+++ xTract_sp_trainingSet.py
@@ -59,7 +59,7 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import SwissProt as sp
 
-def filter_trainingSet_allSpecies(fh_sprot, fh_targets, fh_map,
+def filter_trainingSet_allSpecies_old(fh_sprot, fh_targets, fh_map,
                                   ontType, EXP_default=set([])):
     # Initializes the target_id:
     target_id = int("1"+"0000001")
@@ -109,6 +109,68 @@ def filter_trainingSet_allSpecies(fh_sprot, fh_targets, fh_map,
                 SeqIO.write(outseq_list,fh_targets, "fasta")
                 mapStr = "T" + str(target_id) + '\t' + \
                                str(rec.accessions[0]) + '\n'
+                # Write out the mapping (target id -> protein name):
+                fh_map.write("%s" % mapStr)
+                target_id += 1
+                seqCount_exp += 1
+    return seqCount_exp
+
+def filter_trainingSet_allSpecies(fh_sprot, fh_targets, fh_map,
+                                  ontType, EXP_default=set([])):
+    # Initializes the target_id:
+    target_id = int("1"+"0000001")
+
+    outseq_list = []
+
+    # Counts total number of sequences 
+    # in the sprot file related to the the taxonomy id taxon_id:
+    seqCount = 0
+
+    # Counts total number of sequences in the sprot file related 
+    # to the the taxonomy id taxon_id whose annotations have EXP 
+    # evidence:
+    seqCount_exp = 0
+
+    for rec in sp.parse(fh_sprot):
+        # Selects records that are related to a specific
+        # taxonomy id taxon_id:
+        #if taxon_id in rec.taxonomy_id:
+            exp_code = False 
+            seqCount += 1
+            goTerms = set()
+            # Going over the list of GO information:
+            for crossRef in rec.cross_references: 
+                # Consider the cross_reference entries 
+                # that relate to GO DB:
+                if crossRef[0] == 'GO':
+                    goList = [crossRef[1], 
+                             (crossRef[3].split(':'))[0], 
+                             crossRef[2][0]]
+                    #print (goList[2])
+                    #print(ontType)
+                    #sys.exit(0)
+                    if goList[2] == ontType and \
+                       (crossRef[3].split(':'))[0] in EXP_default:
+                        goTerms.add(goList[0])
+                        exp_code = True
+                        #break
+            # If the protein has no EXP evidence,
+            # write the sequence to the output file:
+            #if not exp_code:
+            if exp_code:
+                goTerms = ','.join(list(goTerms))
+                outseq = SeqRecord(Seq(rec.sequence),
+                                   id="T"+str(target_id),
+                                   description = "%s" %
+                                   (rec.accessions[0]))
+                outseq_list = [outseq]
+                # Write out the sequence:
+                SeqIO.write(outseq_list,fh_targets, "fasta")
+                #mapStr = "T" + str(target_id) + '\t' + \
+                #               str(rec.accessions[0]) + '\n'
+                mapStr = "T" + str(target_id) + '\t' + \
+                               str(rec.accessions[0]) + '\t' + \
+                               goTerms + '\n'
                 # Write out the mapping (target id -> protein name):
                 fh_map.write("%s" % mapStr)
                 target_id += 1
@@ -171,14 +233,71 @@ def filter_trainingSet_singleSpecies(fh_sprot, taxon_id, fh_targets, fh_map,
         if taxon_id in rec.taxonomy_id:
             exp_code = False 
             seqCount += 1
+            goTerms = set()
             # Going over the list of GO information:
             for crossRef in rec.cross_references: 
                 # Consider the cross_reference entries 
                 # that relate to GO DB:
                 if crossRef[0] == 'GO':
-                    goList = [crossRef[1], 
-                             (crossRef[3].split(':'))[0], 
-                             crossRef[2][0]]
+                    goList = [crossRef[1], # GO term
+                             (crossRef[3].split(':'))[0], # Evidence code
+                             crossRef[2][0]] # Ontology symbol
+                    if goList[2] == ontType and \
+                       (crossRef[3].split(':'))[0] in EXP_default:
+                        goTerms.add(goList[0])
+                        exp_code = True
+                        #break
+            # If the protein has no EXP evidence,
+            # write the sequence to the output file:
+            #if not exp_code:
+            if exp_code:
+                goTerms = ','.join(list(goTerms))
+                #print(goTerms)
+                outseq = SeqRecord(Seq(rec.sequence),
+                                   id="T"+str(target_id),
+                                   description = "%s" %
+                                   (rec.accessions[0]))
+                outseq_list = [outseq]
+                # Write out the sequence:
+                SeqIO.write(outseq_list,fh_targets, "fasta")
+                mapStr = "T" + str(target_id) + '\t' + \
+                               str(rec.accessions[0]) + '\t' + \
+                               goTerms + '\n'
+                # Write out the mapping (target id -> protein name):
+                fh_map.write("%s" % mapStr)
+                target_id += 1
+                seqCount_exp += 1
+    return seqCount_exp
+
+def filter_trainingSet_singleSpecies_old(fh_sprot, taxon_id, fh_targets, fh_map,
+                                  ontType, EXP_default=set([])):
+    # Initializes the target_id:
+    target_id = int(taxon_id+"0000001")
+    outseq_list = []
+
+    # Counts total number of sequences 
+    # in the sprot file related to the the taxonomy id taxon_id:
+    seqCount = 0
+
+    # Counts total number of sequences in the sprot file related 
+    # to the the taxonomy id taxon_id whose annotations have EXP 
+    # evidence:
+    seqCount_exp = 0
+
+    for rec in sp.parse(fh_sprot):
+        # Selects records that are related to a specific
+        # taxonomy id taxon_id:
+        if taxon_id in rec.taxonomy_id:
+            exp_code = False 
+            seqCount += 1
+            # Going over the list of GO information:
+            for crossRef in rec.cross_references: 
+                # Consider the cross_reference entries 
+                # that relate to GO DB:
+                if crossRef[0] == 'GO':
+                    goList = [crossRef[1], # GO term
+                             (crossRef[3].split(':'))[0], # Evidence code
+                             crossRef[2][0]] # Ontology symbol
                     if goList[2] == ontType and \
                        (crossRef[3].split(':'))[0] in EXP_default:
                         exp_code = True
@@ -215,6 +334,8 @@ def create_trainingSet_singleSpecies(fh_sprot, taxon_id,
                        trainingFile_LK_mfo_handle,
                        trainingFile_LK_mfo_map_handle,
                        'F', EXP_default)
+    #sys.exit(0) 
+
     print('Creating training set for BPO ontology ..')
     # Repositioning the reference point at the beginning of the file:
     fh_sprot.seek(0)
