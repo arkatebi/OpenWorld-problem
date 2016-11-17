ffd5a756e141053d46e80f820382dcb582937e06
arkatebi <arkatebi@gmail.com>
2016-11-17 11:58:39 -0600

    Script to add misc scripts

--- misc/gitPush_misc.sh
+++ misc/gitPush_misc.sh
@@ -50,8 +50,8 @@ git commit -m 'Script to add figures and data files related to Count_genes progr
 git add ../misc/pushGC_2012to2016.sh 
 git commit -m 'Script to add figures and data files related to Count_genes program' ../misc/pushGC_2012to2016.sh
 
-git add ../misc/genEvalSet-1.sh
-git commit -m 'Script to generate ES-1' ../misc/run_genEvalSet-1.sh
+git add ../misc/genEvalSet_1.sh
+git commit -m 'Script to generate ES-1' ../misc/run_genEvalSet_1.sh
 
 git add ../misc/genEvalSet-2.sh 
 git commit -m 'Script to generate ES-2' ../misc/run_genEvalSet-2.sh
