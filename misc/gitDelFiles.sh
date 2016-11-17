358748313394313c767f437cd3e809e2a2d7796f
arkatebi <arkatebi@gmail.com>
2016-11-17 12:51:31 -0600

    Script to delete files  from git repo

--- misc/gitDelFiles.sh
+++ misc/gitDelFiles.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Five STEPs before running this script:  
+# Four STEPs before running this script:  
 
 # STEP 1: comment out the following line
 #exit 0
@@ -10,12 +10,16 @@ git stash drop
 
 # STEP 3: Delete the file (s):
 # List all the file names to be deleted with the comparative path:
+# To delete file from both git repo and the file system: 
+# git rm filename
+# git commit "-m filename"
+
+# To delete file from only git repo and not remove it from the file system: 
+# git rm --cached filename
+# git commit "-m filename"
 
 #git rm ../genPrecRecCurves.sh
 #git commit -m "remove ../genPrecRecCurves.sh"
-
-#git rm ../gitDel.sh
-#git commit -m "remove ../gitDel.sh"
 
 # STEP 4: Push all the changes to the remote repository: 
 git push origin master 
