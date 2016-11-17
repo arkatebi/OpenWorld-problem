5ffb1f7b7c8a869abd78fadc29e85367d30e82a2
arkatebi <arkatebi@gmail.com>
2016-11-17 11:45:34 -0600

    Script to run CAFAAssess on all evaluation sets

---
+++ misc/genPrecRecCurves.sh
@@ -0,0 +1,29 @@
+#python2 CAFAAssess/precrec_main.py MFO 117 9606 ./CAFAAssess/evalSet-1.mfo.scores.txt.1 ./CAFAAssess/evalSet-1.mfo.png
+#python2 CAFAAssess/precrec_main.py BPO 117 9606 ./CAFAAssess/evalSet-1.bpo.scores.txt.1 ./CAFAAssess/evalSet-1.bpo.png
+#python2 CAFAAssess/precrec_main.py CCO 117 9606 ./CAFAAssess/evalSet-1.cco.scores.txt.1 ./CAFAAssess/evalSet-1.cco.png
+python2 CAFAAssess/precrec_main.py MFO 117 9606 ./workspace/evalSet-1.mfo.scores.txt.1 ./figures-prec/evalSet-1.mfo.png
+python2 CAFAAssess/precrec_main.py BPO 117 9606 ./workspace/evalSet-1.bpo.scores.txt.1 ./figures-prec/evalSet-1.bpo.png
+python2 CAFAAssess/precrec_main.py CCO 117 9606 ./workspace/evalSet-1.cco.scores.txt.1 ./figures-prec/evalSet-1.cco.png
+
+python2 CAFAAssess/precrec_main.py MFO 117 9606 ./workspace/evalSet-2.mfo.scores.txt.1 ./figures-prec/evalSet-2.mfo.png
+python2 CAFAAssess/precrec_main.py BPO 117 9606 ./workspace/evalSet-2.bpo.scores.txt.1 ./figures-prec/evalSet-2.bpo.png
+python2 CAFAAssess/precrec_main.py CCO 117 9606 ./workspace/evalSet-2.cco.scores.txt.1 ./figures-prec/evalSet-2.cco.png
+
+python2 CAFAAssess/precrec_main.py MFO 117 9606 ./workspace/evalSet-3.mfo.scores.txt.1 ./figures-prec/evalSet-3.mfo.png
+python2 CAFAAssess/precrec_main.py BPO 117 9606 ./workspace/evalSet-3.bpo.scores.txt.1 ./figures-prec/evalSet-3.bpo.png
+python2 CAFAAssess/precrec_main.py CCO 117 9606 ./workspace/evalSet-3.cco.scores.txt.1 ./figures-prec/evalSet-3.cco.png
+
+python2 CAFAAssess/precrec_main.py MFO 117 9606 ./workspace/evalSet-4.mfo.scores.txt.1 ./figures-prec/evalSet-4.mfo.png
+python2 CAFAAssess/precrec_main.py BPO 117 9606 ./workspace/evalSet-4.bpo.scores.txt.1 ./figures-prec/evalSet-4.bpo.png
+python2 CAFAAssess/precrec_main.py CCO 117 9606 ./workspace/evalSet-4.cco.scores.txt.1 ./figures-prec/evalSet-4.cco.png
+
+python2 CAFAAssess/precrec_main.py MFO 117 9606 ./workspace/evalSet-5.mfo.scores.txt.1 ./figures-prec/evalSet-5.mfo.png
+python2 CAFAAssess/precrec_main.py BPO 117 9606 ./workspace/evalSet-5.bpo.scores.txt.1 ./figures-prec/evalSet-5.bpo.png
+python2 CAFAAssess/precrec_main.py CCO 117 9606 ./workspace/evalSet-5.cco.scores.txt.1 ./figures-prec/evalSet-5.cco.png
+
+python2 CAFAAssess/precrec_main.py MFO 117 9606 ./workspace/evalSet-6.mfo.scores.txt.1 ./figures-prec/evalSet-6.mfo.png
+python2 CAFAAssess/precrec_main.py BPO 117 9606 ./workspace/evalSet-6.bpo.scores.txt.1 ./figures-prec/evalSet-6.bpo.png
+python2 CAFAAssess/precrec_main.py CCO 117 9606 ./workspace/evalSet-6.cco.scores.txt.1 ./figures-prec/evalSet-6.cco.png
+
+# Note: though this scores are on all organisms, the group name and organism name is supplied to comply with the CAFAAssess program
+
