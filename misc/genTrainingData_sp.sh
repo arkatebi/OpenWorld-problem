aa713970894bebd2014c45efb2719fa71554f12f
arkatebi <arkatebi@gmail.com>
2016-11-17 12:55:26 -0600

    Script to generate ES-1

---
+++ misc/genTrainingData_sp.sh
@@ -0,0 +1,12 @@
+date 
+python xTract_trainingSet -I1=uniprot_sprot.dat.2010_01 -G=9606 -O=trainingSet
+python xTract_trainingSet -I1=uniprot_sprot.dat.2010_01 -G=10090 -O=trainingSet
+python xTract_trainingSet -I1=uniprot_sprot.dat.2010_01 -G=3702 -O=trainingSet
+python xTract_trainingSet -I1=uniprot_sprot.dat.2010_01 -G=10116 -O=trainingSet
+python xTract_trainingSet -I1=uniprot_sprot.dat.2010_01 -G=559292 -O=trainingSet
+python xTract_trainingSet -I1=uniprot_sprot.dat.2010_01 -G=9913 -O=trainingSet
+python xTract_trainingSet -I1=uniprot_sprot.dat.2010_01 -G=284812 -O=trainingSet
+python xTract_trainingSet -I1=uniprot_sprot.dat.2010_01 -G=83333 -O=trainingSet
+python xTract_trainingSet -I1=uniprot_sprot.dat.2010_01 -G=224308 -O=trainingSet
+python xTract_trainingSet -I1=uniprot_sprot.dat.2010_01 -G=44689 -O=trainingSet
+date 
