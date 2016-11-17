dae169ce94c77aba7a7e76b76b7168b4582ee1dd
arkatebi <arkatebi@gmail.com>
2016-11-17 12:32:23 -0600

    remove ../genEvalSet-2.sh

--- genEvalSet-2.sh
+++
@@ -1,9 +0,0 @@
-date
-python xTract_reevalSet -I1=evalSet-1.mfo.map -I2=uniprot_sprot.dat.2012_01 -N=F -O=evalSet-2
-echo 'DONE'
-date 
-python xTract_reevalSet -I1=evalSet-1.bpo.map -I2=uniprot_sprot.dat.2012_01 -N=P -O=evalSet-2
-echo 'DONE'
-date
-python xTract_reevalSet -I1=evalSet-1.cco.map -I2=uniprot_sprot.dat.2012_01 -N=C -O=evalSet-2
-date
