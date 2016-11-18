ce465c2b625c157fe8f38ad3c33cefb36cfb3d3c
arkatebi <arkatebi@gmail.com>
2016-11-18 10:26:01 -0600

    Script to add misc scripts

--- misc/gitPush_misc.sh
+++ misc/gitPush_misc.sh
@@ -95,6 +95,9 @@ git commit -m 'Script to generate ES-6 for species' ../misc/genEvalSet-6-sp.sh
 git add ../misc/countTerms.gawk 
 git commit -m 'Script to count strings in a text file' ../misc/countTerms.gawk
 
+git add ../misc/gitPush_CAFAAssess.sh 
+git commit -m 'Script to generate ES-6 for species' ../misc/gitPush_CAFAAssess.sh
+
 git remote add origin https://github.com/arkatebi/OpenWorld-problem
 #git pull origin master
 git push origin master
