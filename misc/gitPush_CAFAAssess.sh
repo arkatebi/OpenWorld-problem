02d171c412a54c829331589e1ed7f0dd3a250b0a
arkatebi <arkatebi@gmail.com>
2016-11-23 13:24:15 -0600

    Script to add CAFAAssess folder

--- misc/gitPush_CAFAAssess.sh
+++ misc/gitPush_CAFAAssess.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-git config --global user.email arkatebi@gmail.com
-git config --global user.name arkatebi
+#git config --global user.email arkatebi@gmail.com
+#git config --global user.name arkatebi
 
-git remote rm origin
+#git remote rm origin
 
-git add ../CAFAAssess
-git commit -m 'Added CAFAAsses folder' ../CAFAAssess
+#git add ../CAFAAssess
+#git commit -m 'Added CAFAAsses folder' ../CAFAAssess
 
-git remote add origin https://github.com/arkatebi/OpenWorld-problem
+git add ../CAFAAssess/precrec_main.py
+git commit -m 'Added CAFAAsses folder' ../CAFAAssess/precrec_main.py
+
+#git remote add origin https://github.com/arkatebi/OpenWorld-problem
 git push origin master
 
 # Discard unstaged changes:
-git stash save --keep-index
-git stash drop
+#git stash save --keep-index
+#git stash drop
