2fdde3c529653ec6fbfc7106dc1b5cd18abe5a17
arkatebi <arkatebi@gmail.com>
2016-11-29 14:16:54 -0600

    Script to update workspace folder

--- misc/gitPush_workspace.sh
+++ misc/gitPush_workspace.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-#git config --global user.email arkatebi@gmail.com
-#git config --global user.name arkatebi
-
-#git remote rm origin
 
 #git add ../workspace/evalSets
 #git commit -m 'Added folder for evaluation sets' ../workspace/evalSets
@@ -11,7 +7,7 @@
 #git commit -m 'Added folder for evaluation sets' ../workspace/benchmarks
 
 #git remote add origin https://github.com/arkatebi/OpenWorld-problem
-git push origin master
+#git push origin master
 
 # Discard unstaged changes:
 #git stash save --keep-index
