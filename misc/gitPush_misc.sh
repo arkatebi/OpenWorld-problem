6ec01aaeebf71561cbf15223da4768f1cefddff2
arkatebi <arkatebi@gmail.com>
2016-12-01 12:02:40 -0600

    Script to update misc folder

--- misc/gitPush_misc.sh
+++ misc/gitPush_misc.sh
@@ -17,14 +17,17 @@ git commit -m 'Script to run CAFAAssess on all evaluation sets' ../misc/genPrecR
 git add ../misc/gitPush_trainingSet.sh 
 git commit -m 'Script to add files related to xTract_trainingSet program' ../misc/gitPush_trainingSet.sh
 
-git add ../misc/gitDel.sh 
-git commit -m 'Script to delete a file or directory from git repo' ../misc/gitDel.sh
+#git add ../misc/gitDel.sh 
+#git commit -m 'Script to delete a file or directory from git repo' ../misc/gitDel.sh
+
+git rm ../misc/gitDel.sh 
+git commit -m 'Delete script from git repo' ../misc/gitDel.sh
 
 #git add ../misc/gitDelFiles.sh 
 #git commit -m 'Script to delete files  from git repo' ../misc/gitDelFiles.sh
 
-git rm  ../misc/gitDelFiles.sh 
-git commit -m 'Delete the script from git repo' ../misc/gitDelFiles.sh
+#git rm  ../misc/gitDelFiles.sh 
+#git commit -m 'Delete the script from git repo' ../misc/gitDelFiles.sh
 
 git add ../misc/gitPush_assignBlastScores.sh 
 git commit -m 'Script to add files related to Assign_blastScores program' ../misc/gitPush_assignBlastScores.sh 
