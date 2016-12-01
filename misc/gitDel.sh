606d7056277f285925829986b86d2384f55fb733
arkatebi <arkatebi@gmail.com>
2016-12-01 11:39:50 -0600

    Script to delete a file or directory from git repo

--- misc/gitDel.sh
+++ misc/gitDel.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
-# Five STEPs before running this script:  
 
-# STEP 1: comment out the following line
-#exit 0
-
-# STEP 2: Discard unstaged changes:
-git stash save --keep-index
-git stash drop
+# Discard unstaged changes:
+#git stash save --keep-index
+#git stash drop
 
 # Delete the file or the folder:
 
-# STEP 3: Replace FOLDERNAME with the file or folder to be deleted from the git repository:
+# Replace FOLDERNAME with the file or folder to be deleted from the git repository:
 #git filter-branch -f --index-filter "git rm -rf --cached --ignore-unmatch FOLDERNAME" -- --all
 #git filter-branch -f --index-filter "git rm -rf --cached --ignore-unmatch Ontology" -- --all
-#git filter-branch -f --index-filter "git rm -rf --cached --ignore-unmatch workspace/geneCount.stat.1.xls" -- --all
 
-# STEP 4: Clean up the local repository:
-rm -rf ./git/refs/original/
-git reflog expire --expire=now --all
-git gc --prune=now
-git gc --aggressive --prune=now
+git rm -r ../workspace/benchmarks
+git commit -m 'Removed folder ../workspace/benchmarks' 
 
-# STEP 5: Push all the changes to the remote repository: 
-git push --all --force
+# Clean up the local repository:
+#rm -rf ./git/refs/original/
+#git reflog expire --expire=now --all
+#git gc --prune=now
+#git gc --aggressive --prune=now
 
+# Push all the changes to the remote repository: 
+#git push --all --force
+git push origin master 
