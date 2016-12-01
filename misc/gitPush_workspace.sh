7c6c53b365ca0dadd60980f1f4799067f310733a
arkatebi <arkatebi@gmail.com>
2016-12-01 11:39:50 -0600

    Script to update workspace folder

--- misc/gitPush_workspace.sh
+++ misc/gitPush_workspace.sh
@@ -3,11 +3,14 @@
 #git add ../workspace/evalSets
 #git commit -m 'Added folder for evaluation sets' ../workspace/evalSets
 
-#git add ../workspace/benchmarks
-#git commit -m 'Added folder for evaluation sets' ../workspace/benchmarks
+#git rm -r ../workspace/evalSets
+#git commit -m 'Removed folder evalSets' ../workspace/evalSets
+
+#git add ../workspace/blastScores
+#git commit -m 'Added folder for blast scores' ../workspace/blastScores
 
 #git remote add origin https://github.com/arkatebi/OpenWorld-problem
-#git push origin master
+git push origin master
 
 # Discard unstaged changes:
 #git stash save --keep-index
