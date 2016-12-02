ce6ce323b07f8675eb370e09bbe1b5309be2dd84
arkatebi <arkatebi@gmail.com>
2016-12-02 12:58:18 -0600

    Script for computing precision-recall

--- misc/gitPush_PRcurves.sh
+++ misc/gitPush_PRcurves.sh
@@ -13,14 +13,17 @@ git commit -m 'Simplified' ../CAFAAssess/precrec_main.py
 git add ../CAFAAssess/precRec.py
 git commit -m 'Modified method read_benchamrk' ../CAFAAssess/precRec.py
 
-git add ../figures/prCurves
-git commit -m 'Added subfolder figures/prCurves' ../figures/prCurves
+git add ../CAFAAssess/README.md
+git commit -m 'Updated README file' ../CAFAAssess/README.md
 
-git add ../genPRcurves.sh
-git commit -m 'Added script to generate all prCurves' ../genPRcurves.sh
+#git add ../figures/prCurves
+#git commit -m 'Added subfolder figures/prCurves' ../figures/prCurves
 
-git add ../genPRcurves.txt
-git commit -m 'Added message file from cript genPRcurves.sh' ../genPRcurves.txt
+#git add ../genPRcurves.sh
+#git commit -m 'Added script to generate all prCurves' ../genPRcurves.sh
+
+#git add ../genPRcurves.txt
+#git commit -m 'Added message file from cript genPRcurves.sh' ../genPRcurves.txt
 
 git remote add origin https://github.com/arkatebi/OpenWorld-problem
 git push origin master
